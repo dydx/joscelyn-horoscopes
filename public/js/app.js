8a9e0a896dd9ad26129a9a3e325dafca3d6c995d
Joshua Sandlin <josh.sandlin@turner.com>
2017-01-20 02:39:46 -0500

    added comments to clarify anything that might be confusing

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,34 @@
 (function () {
+  /**
+   * getting the dropdown list on line 20 of index.html
+   */
   const signs = document.getElementById('signs');
+
+  /**
+   * getting the message box on line 39 of index.html
+   */
   const display = document.getElementById('horoscope');
+
+  /**
+   * Keeping the URL to our API out of the logic
+   */
   const API_URL = 'http://localhost:3000/api';
 
-  signs.addEventListener('change', function (event) {
-    let sign = event.target.value;
-    fetchHoroscope(sign);
+  /**
+   * Here we are adding an event listener to the dropdown list
+   * so that we may use the provided value to query the API
+   */
+  signs.addEventListener('change', function (event) { // the event is 'change'
+    let sign = event.target.value;                    // we are getting the users value
+    fetchHoroscope(sign);                             // we are fetching the horoscope from the API
   });
 
+  /**
+   * function used to query the API for data. Does use a little ES6 notation.
+   *
+   * Biggest take-away from this is that it is using the Fetch API:
+   * > https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+   */
   function fetchHoroscope (sign) {
     fetch(`${API_URL}/${sign}`)
       .then(res => res.text())
@@ -15,6 +36,11 @@
       .catch(displayError);
   }
 
+  /**
+   * Here we are removing the hidden class from the message box
+   * and then filling it with the API response's text
+   *
+   */
   function displayText (text) {
     document.querySelector('.message').classList.remove('hidden');
     display.textContent = text;
@@ -22,6 +48,9 @@
     return text;
   }
 
+  /**
+   * In case something goes wrong, we can tell the user
+   */
   function displayError (err) {
     displayText(`There was an error: ${err}`);
   }
